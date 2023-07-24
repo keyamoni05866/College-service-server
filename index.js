7f2a6882bc6199e719da04e2094a45c02f369eaa
keyamoni05866 <keya05866@gmail.com>
2023-07-24 21:37:54 +0600

    last commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 const collegeCollection = client.db('collegService').collection('colleges')
 const admissionCollection = client.db('collegService').collection('admission')
 
@@ -61,8 +61,8 @@ app.get("/myCollege", async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
